4f27cb74a8f2def6e52c159125d135e4fe0c05e2
Bhushan <bhushangaykawad@gmail.com>
2019-12-24 15:34:34 +0530

    admin-lte added designed login page with fontawesome

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -34,8 +34,8 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    @include('layouts._head')
+</head>
+
+<body class="login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <img src="{{ asset('logo.png') }}" alt="{{ config('app.name', 'Laravel') }}" height="150">
+        </div>
+
+        <div class="card">
+            <div class="card-body login-card-body">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+</body>
+
+</html>
