d4b4c7bf1a91c1db948d4cce7ad0ca5d989ab2b5
Bhushan Gaikwad <bhushangaykawad@gmail.com>
2020-04-30 13:54:33 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,9 @@ use Intervention\Image\Facades\Image;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
 
     /**
      * Crop and Store Image.
